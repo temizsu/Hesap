ea7bb1773f2dc3b39ce352b6d029a0a49d90cc35
temizsu <49598455+temizsu@users.noreply.github.com>
2020-02-26 00:24:58 +0300

    test1

--- src/app/App.java
+++ src/app/App.java
@@ -4,6 +4,8 @@ public class App {
 
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java");
+
+        System.out.println("Hello Java");
         System.out.println("Hello Java");
     }
 }

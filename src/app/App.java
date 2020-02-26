b1ea91ec1d70f9729f57d7803ca4ad8244e17a58
temizsu <49598455+temizsu@users.noreply.github.com>
2020-02-26 21:42:34 +0300

    test1

--- src/app/App.java
+++ src/app/App.java
@@ -9,5 +9,6 @@ public class App {
         System.out.println("Hello Java");
         System.out.println("Hello Java");
         System.out.println("Hello Java");
+        System.out.println("Hello Java");
     }
 }
